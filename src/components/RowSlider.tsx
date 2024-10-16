a316ba2cf3525907821d31d3fe1136f58f16d7f4
stoerie <stoerie@stoeriestories.de>
2024-10-16 13:17:55 +0200

    chore: start improving sliders

--- src/components/RowSlider.tsx
+++ src/components/RowSlider.tsx
@@ -4,10 +4,11 @@ export const RowSlider: Component<Record<string, unknown>> = (props) => {
   return (
     <p class="miru--menu__row">
       {/* {props.effect} */}
-      <span class="miru--slider-label">
-        {props.label}: {props.value}%
-      </span>
+      {/* i-tabler:arrow-badge-down-filled */}
+      <div class="miru--slider-reset"></div>
+      <span class="miru--slider-label">{props.label}:</span>
       <input type="range" step="0.01" class="miru--slider" {...props} />
+      <span class="miru--slider-label">{props.value}</span>
     </p>
   )
 }
