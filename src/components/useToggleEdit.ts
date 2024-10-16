54164c3d5e891a3af735ce6a10d397a7694dde20
Taye Adeyemi <dev@taye.me>
2024-10-16 22:45:07 +0200

    fix: add useToggleEdit.ts

---
+++ src/components/useToggleEdit.ts
@@ -0,0 +1,32 @@
+import { ImageSourceState } from '@/engine/ImageSourceState'
+import { MaybeRefOrGetter, ref, toRef, toValue, watch } from '@/framework/reactivity'
+import { ImageEditState } from '@/types'
+
+export const useTogleEdit = <T extends keyof ImageEditState>(
+  source: MaybeRefOrGetter<ImageSourceState | undefined>,
+  key: T,
+) => {
+  const savedValue = ref<ImageEditState[T]>()
+
+  // clear prev toggle value on source change
+  watch([toRef(source)], () => (savedValue.value = undefined))
+
+  const toggle = () => {
+    const $source = toValue(source)
+    if (!$source) return
+
+    if (savedValue.value) {
+      $source[key].value = savedValue.value
+      clearSavedValue()
+    } else {
+      savedValue.value = $source[key].value as ImageEditState[T]
+      $source[key].value = undefined
+    }
+  }
+
+  const clearSavedValue = () => {
+    savedValue.value = undefined
+  }
+
+  return { toggle, clearSavedValue, hasSavedValue: toRef(() => !!savedValue.value) }
+}
