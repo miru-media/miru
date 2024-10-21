8acb3bc0997cf034c6c5ef5a3ad784c5f99fe9d7
Taye Adeyemi <dev@taye.me>
2024-10-21 18:37:46 +0200

    chore(framework): add jsx-dev-runtime

---
+++ src/framework/jsx-dev-runtime.ts
@@ -0,0 +1,3 @@
+import { Fragment, jsx } from './jsx-runtime'
+
+export { jsx as jsxDEV, Fragment }
--- vite.config.ts
+++ vite.config.ts
@@ -31,7 +31,6 @@ export default defineConfig({
   esbuild: {
     jsx: 'automatic',
     jsxImportSource: '@/framework',
-    jsxDev: false,
   },
   base: './',
   build: {
