dfca1c224720780739e0a2e721a92f66b3e60944
stoerie <stoerie@stoeriestories.de>
2024-10-22 15:06:12 +0200

    chore: start unifying sliders with reset in the back

--- src/components/RowSlider.tsx
+++ src/components/RowSlider.tsx
@@ -10,18 +10,21 @@ export const RowSlider: Component<{
   toggleContext?: ReturnType<typeof useTogleEdit>
 }> = ({ toggleContext, label, value, ...inputProps }) => {
   toggleContext = toValue(toggleContext)
-  const hasToggle = !!toggleContext
+  // const hasToggle = !!toggleContext
 
   return (
     <p class="miru--menu__row">
-      <button disabled={!hasToggle} class="miru--button miru--small" onClick={toggleContext?.toggle}>
-        {() => toggleContext?.hasValue.value && <IconTablerCircleOff class="miru--button__icon" />}
+      <button class="miru--button miru--small">
+        {/* {() => toggleContext?.hasValue.value && <IconTablerCircleOff class="miru--button__icon" />} */}
         <label class="miru--button__label">{label}</label>
       </button>
 
       <input type="range" step="0.01" class="miru--slider" {...inputProps} value={value} />
 
-      <button class="miru--button miru--small" disabled>
+      <button
+        class={() => ['miru--button', 'miru--small', toggleContext?.isToggledOff.value && 'miru--mark-disabled']}
+        onClick={toggleContext?.toggle}
+      >
         <label class="miru--button__label">{value}</label>
       </button>
     </p>
