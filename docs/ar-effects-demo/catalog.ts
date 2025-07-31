aed5f2db7b7ad35f4310bdb81e16e6a454129012
Taye Adeyemi <dev@taye.me>
2025-07-31 21:21:31 +0200

    docs: update effects catalog

--- docs/ar-effects-demo/catalog.ts
+++ docs/ar-effects-demo/catalog.ts
@@ -1,17 +1,19 @@
 import Beard from 'https://assets.miru.media/gltf/effects/Beard.glb'
+import Cat from 'https://assets.miru.media/gltf/effects/Cat.glb'
 import Clown from 'https://assets.miru.media/gltf/effects/Clown.glb'
 import Eyes from 'https://assets.miru.media/gltf/effects/Eyes.glb'
 import Makeup from 'https://assets.miru.media/gltf/effects/Makeup.glb'
-import Rain from 'https://assets.miru.media/gltf/effects/Rain.glb'
-import Teddy from 'https://assets.miru.media/gltf/effects/Teddy.glb'
-import Teeth from 'https://assets.miru.media/gltf/effects/Teeth.glb'
+import Rain from 'https://assets.miru.media/gltf/effects/Rain2.glb'
+import Teddy2 from 'https://assets.miru.media/gltf/effects/Teddy_Fur.glb'
+import Teddy1 from 'https://assets.miru.media/gltf/effects/TeddyFabric.glb'
 
 export const catalog = [
+  { name: 'Cat', url: Cat },
   { name: 'Stash', url: Beard },
   { name: 'Clown', url: Clown },
   { name: 'Eyes', url: Eyes },
   { name: 'Make-up', url: Makeup },
   { name: 'Rain', url: Rain },
-  { name: 'Teddy', url: Teddy },
-  { name: 'Teeth', url: Teeth },
+  { name: 'Teddy 1', url: Teddy1 },
+  { name: 'Teddy 2', url: Teddy2 },
 ]
